66eae5a9a5b712c7bf3e39ce9ebded075c1b6513
Mikael LE MENTEC <mikeleyeti@yahoo.fr>
2020-04-18 15:22:13 +0200

    Commit initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			texteInitial: 'test',
+			usedLetters: 't',
+		}
+	}
+
+	affichage() {
+		const phraseDevinette = this.state.texteInitial
+		const usedLetters = this.state.usedLetters
+
+		// return phraseDevinette.replace(usedLetters,'_')
+		function computeDisplay(phrase, usedLetters) {
+			return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'))
+		}
+
+		return computeDisplay(phraseDevinette,usedLetters)
+
+	}
+
+	render() {
+		return (
+			<div>
+				Ceci est un {this.affichage()}
+			</div>
+		);
+	}
 }
 
 export default App;
