7c380bce25a790c05808bba43584fcc27a4f1111
Mikael LE MENTEC <mikeleyeti@yahoo.fr>
2020-04-18 17:51:19 +0200

    Fonction d'affichage

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 
 class App extends React.Component {
 	constructor(props) {
+		var test = new Set(["t",]);
 		super(props);
 		this.state = {
 			texteInitial: 'test',
-			usedLetters: 't',
+			usedLetters: test,
 		}
 	}
 
@@ -15,13 +16,14 @@ class App extends React.Component {
 		const phraseDevinette = this.state.texteInitial
 		const usedLetters = this.state.usedLetters
 
-		// return phraseDevinette.replace(usedLetters,'_')
+		// Produit une représentation textuelle de l’état de la partie,
+		// chaque lettre non découverte étant représentée par un _underscore_.
+		// (CSS assurera de l’espacement entre les lettres pour mieux// visualiser le tout).
 		function computeDisplay(phrase, usedLetters) {
 			return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'))
 		}
 
-		return computeDisplay(phraseDevinette,usedLetters)
-
+		return computeDisplay(phraseDevinette, usedLetters)
 	}
 
 	render() {
@@ -34,3 +36,4 @@ class App extends React.Component {
 }
 
 export default App;
+
