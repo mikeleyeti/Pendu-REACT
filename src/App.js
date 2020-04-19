8ce18233b0242413c4f6bb2f73f0c06a696a96d3
Mikael LE MENTEC <mikeleyeti@yahoo.fr>
2020-04-19 19:16:34 +0200

    Gestion de la fin du jeu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './App.css';
 import {AffichageTouche} from "./Touche";
+import {Score} from "./Score";
 
 // Construction des 26 lettres de l'alphabet à partir des codes ASCII.
 const alphabet = [];
 for (let i = 65; i <= 90; i++) {
 	alphabet.push(String.fromCharCode(i));
 }
+
+const nombreMaximumTentatve = 10
 
 
 class App extends React.Component {
@@ -17,14 +20,30 @@ class App extends React.Component {
 		this.state = {
 			texteInitial: 'test'.toUpperCase(),
 			usedLetters: lettres,
+			nombreTentatives: 0,
+			jouer: true,
 		}
 		// Bind de handleClick pour conserver le this
 		this.handleClick = this.handleClick.bind(this)
 	}
 
+	calculScore(phraseInitiale, lettresTrouvees) {
+		let score = 0
+		for (let lettre of phraseInitiale) {
+			if (lettresTrouvees.has(lettre)) {
+				score += 1
+			}
+		}
+		return score
+	}
+
 	handleClick(index) {
 		// Ajoute la lettre cliquée dans le set usedLetters
-		this.setState({usedLetters: this.state.usedLetters.add(index)})
+		this.state.jouer && this.setState({usedLetters: this.state.usedLetters.add(index)})
+		if (!this.state.texteInitial.includes(index)) {
+			this.state.jouer && this.setState({nombreTentatives: this.state.nombreTentatives + 1})
+			this.state.nombreTentatives == nombreMaximumTentatve - 1 && this.setState({jouer: false})
+		}
 	}
 
 	affichagePhrase(phraseDevinette, usedLetters) {
@@ -43,14 +62,12 @@ class App extends React.Component {
 		// Récupération de l'état local.
 		const {texteInitial, usedLetters} = this.state
 
-		function checkEndGame() {
-			return !!usedLetters.has('T');
-		}
-
 		return (
 			<div>
+				Nombre de tentatives : {this.state.nombreTentatives} <br/>
 				Mot à trouver : {this.affichagePhrase(texteInitial, usedLetters)} <br/>
-				{checkEndGame() ? "C'est fini" : "C'est pas encore fini."}
+				{/*{checkEndGame() ? "C'est fini" : "C'est pas encore fini."}*/}
+				{this.state.jouer ? false : "C'est perdu !"}
 				<div>
 					{/* Affichage d'un clavier avec les 26 lettres de l'alphabet. */}
 					{alphabet.map((lettre, index,) => (
--- src/Touche.js
+++ src/Touche.js
@@ -1,10 +1,9 @@
 import React from "react";
+// Créer un bouton de props lettre et onClick
+// onClick est une fonction qui sera un gestionnaire de click prenant en paramètre la prop lettre
 
-export const AffichageTouche = (
-	// Créer un bouton de props lettre et onClick
-	// onClick est une fonction qui sera un gestionnaire de click prenant en paramètre la prop lettre
-	{touche: lettre,onClick}) => (
-		<button onClick={() => onClick(lettre)}>{lettre}</button>
+export const AffichageTouche = ({touche: lettre, onClick}) => (
+	<button onClick={() => onClick(lettre)}>{lettre}</button>
 )
 
 
